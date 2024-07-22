0585649b809222dcdd9057db7a702f4f66c6d9f0
AndrianusAI <artificialandrianus@gmail.com>
2024-07-22 14:38:07 +0700

    first commit

---
+++ src/utils/api/variables.js
@@ -0,0 +1,8 @@
+export const variables = {
+    BASE_URL: "https://7cc9l4t7-7089.asse.devtunnels.ms/api/",
+    USER_API: "https://7cc9l4t7-7089.asse.devtunnels.ms/api/user",
+    PRODUCT_API: "https://7cc9l4t7-7089.asse.devtunnels.ms/api/product",
+    ORDER_API: "https://7cc9l4t7-7089.asse.devtunnels.ms/api/order",
+    PRODUCTSIZE_API: "https://7cc9l4t7-7089.asse.devtunnels.ms/api/productsize",
+    ORDERITEM_API: "https://7cc9l4t7-7089.asse.devtunnels.ms/api/orderitem",
+}
